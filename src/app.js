be97eea6725042471eedf9d5ff54d47e0ddf5d09
prashantdawar <prashantdawar.2008@gmail.com>
2019-01-24 14:06:56 +0530

    update

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@
 let router = require('./router');
 let server = require('http').createServer();
 let hostname = process.env.OPENSHIFT_NODEJS_IP;
-let port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT|| 3001;
+let port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 3001;
 server.on('request', (req, res) => router(req, res))
 .listen(port, hostname,  () => console.log(`Server is running at PORT: ${port}`));
 module.exports = server;
